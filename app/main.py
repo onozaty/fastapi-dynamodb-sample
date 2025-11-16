b8dd3c7a20e26c3d549249852066743f6b9f3939
onozaty <onozaty@gmail.com>
2025-11-17 00:00:18 +0900

    /ping -> /health に変更

--- app/main.py
+++ app/main.py
@@ -19,9 +19,9 @@ app = FastAPI()
 app.include_router(items.router)
 
 
-@app.get("/ping")
-async def ping() -> dict[str, str]:
-    """ヘルスチェック用のpingエンドポイント。"""
+@app.get("/health")
+async def health() -> dict[str, str]:
+    """ヘルスチェック用エンドポイント。"""
     return {"status": "ok"}
 
 
